e42dda71e26fcb5ca4650d523d71d51f52777e4c
Marco Romero - note do trabalho <mmcromero@gmail.com>
2018-03-05 03:10:04 -0300

    V2

--- JARBAS-INREBRA-2018-V2.ino/JARBAS-INREBRA-2018-V2.ino.ino
+++ JARBAS-INREBRA-2018-V2.ino/JARBAS-INREBRA-2018-V2.ino.ino
@@ -42,8 +42,6 @@ String cmd = "";
 String comando = "";
 String strTipo = "";
 String strRepeticao = "";
-String cmdEthernet = "";
-String cmdWire = "";
 String strLocal = "N"; 
 
 String strTamanho = ""; 
@@ -231,12 +229,6 @@ void setup(){
 
   Ethernet.begin(myMac, myIp, gateway, subnet);
 
-  if(ambiente == 0){ // TESTE
-    //apresentaLuzes();
-  }else if(ambiente == 1){ //PRODUÇÃO
-    
-  }
-
 
   //Rec IR
   irrecv.enableIRIn(); // Inicializa o receptor IR 
@@ -292,134 +284,64 @@ void loop(){
 
 
   /////// RECEBE ETHERNET //-----------------------------------------------------------------------------------------------------------
-  EthernetClient client1 = server.available();
-  if (client1) {
-    boolean currentLineIsBlank = true;
-    while (client1.connected()) {
-      if (client1.available()) {
-        char c = client1.read();
-        if (c != ' ') {
-          cmdEthernet += c;
-          Serial.println(c);
-        }else{
-          Serial.println("espaço em branco");
-        }
-        
+  EthernetClient client = server.available();
+  if (!client) {
+    return;
+  }
 
-        if (c == '\n' && currentLineIsBlank) {
-          
-          client1.println("HTTP/1.1 200 OK");
-          // IMPORTANTE, ISSO FAZ O ARDUINO RECEBER REQUISIÇÃO AJAX DE OUTRO SERVIDOR E NÃO APENAS LOCAL.
-          client1.println("Content-Type: text/javascript");
-          client1.println("Access-Control-Allow-Origin: *");
-          client1.println();          
-          //montaString();
-          //client1.print(montaString());
-          break;
-          
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } 
-        else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-
-    if (cmdEthernet.indexOf("/IR") != -1)  {
-          strTipo = cmdEthernet.substring(8,6);
-          strRepeticao = cmdEthernet.substring(8,9);
-          intTamanho = cmdEthernet.length();
-          strLocal = cmdEthernet.substring(intTamanho -9, intTamanho -10);
-          comando = cmdEthernet.substring(9,intTamanho -10);
-      
-          Serial.print("strTipo: ");
-          Serial.println(strTipo);
-          Serial.print("strRepeticao: ");
-          Serial.println(strRepeticao);
-          Serial.print("strLocal: ");
-          Serial.println(strLocal);
-          Serial.print(" ");
-          Serial.print(" request:");
-          Serial.println(cmdEthernet);
-      
-          //sendIr(strRepeticao,strLocal, comando);
-          
-    }
-    
-    // give the web browser time to receive the data
+  // Wait until the client sends some data
+  Serial.println("Novo client");
+  while(!client.available()){
     delay(1);
-    // close the connection:
-    client1.stop();
   }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.print("Request: ");
+  Serial.println(request);
+  
+  if (request.indexOf("/#") != -1)  {
+    strTipo = request.substring(8,6);
+    strRepeticao = request.substring(8,9);
+    intTamanho = request.length();
+    strLocal = request.substring(intTamanho -9, intTamanho -10);
+    comando = request.substring(9,intTamanho -10);
+
+    Serial.print("strTipo: ");
+    Serial.println(strTipo);
+    Serial.print("strRepeticao: ");
+    Serial.println(strRepeticao);
+    Serial.print("strLocal: ");
+    Serial.println(strLocal);
+    Serial.print("comando: ");
+    Serial.println(comando);
+
+    //sendIr(strRepeticao,strLocal, comando);
+    
+  }
+    
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Access-Control-Allow-Origin: *");
+  client.println(""); //  do not forget this one
+  /*
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<br><br>");
+  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
+  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
+  client.println("</html>");
+ */
+  delay(1);
+  client.stop();
+  Serial.println("Client disconectado");
+  Serial.println("");
+    
+    
+  
   //-----------------------------------------------------------------------------------------------------------------------------
   
-  /////// RECEBE SERIAL //-----------------------------------------------------------------------------------------------------------
-  if(Serial.available() > 0) // serial pc
-  {
-    
-    while(Serial.available() > 0)
-    {
-      cmd += char(Serial.read());
-      delay(10);
-    }
-    Serial.println(cmd); 
-    //refHora = millis();
-  }
-  
-  if (cmd.length() >0 || cmdEthernet.length() >0) { 
-    cmd += cmdEthernet;
-    
-    // lampadas
-    // informa tempera
-    //if(cmd == "t1"){dht11Externo();}
-    //if(cmd == "t2"){dht11Sala();}
-  
-    //if(cmd == "relogio"){  mostraHora();}
-    
-
-    strTipo = cmd.substring(0,1);
-    strRepeticao = cmd.substring(1,2);
-    strLocal = cmd.substring(cmd.length() -1);
-    comando = cmd.substring(2,cmd.length() -1);
-    
-    //comandos IR
-    
-    if(strTipo == "#"){
-      sendIr(strRepeticao,strLocal, comando);
-    }
-
-    
-    cmd = "";
-    comando = "";
-    cmdEthernet = "";
-  }
-  //-----------------------------------------------------------------------------------------------------------------------------
-
-
-  // Executa 1 vez a cada 15 segundos
-  //if(millis() - ref >= 15000){
-    //dht11Externo();
-    //dht11Sala();
-    //ref = millis(); // Atualiza a referência
-  //}
-
-  
-  //if(millis() - refHora >= 1000){
-  //  verificaRtc();
-  //  refHora = millis(); // Atualiza a referência
-  //}
   
 }// FIM DO LOOP  
-
-///////////////////////////////// funções
-
-
-
-//-----------------------------------------------------------------------------------------------------------------------------
-
-
 
